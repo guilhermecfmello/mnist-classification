36751e2b775e6650c0a0dd081d6f89eeb9366bfe
Guilherme Mello <guilhermecfmello@gmail.com>
2020-06-16 21:22:18 -0300

    first-commit

---
+++ classificator.py
@@ -0,0 +1,84 @@
+import tensorflow as tf
+import tensorflow_quantum as tfq
+
+import cirq
+import sympy
+import numpy as np
+import seaborn as sns
+import collections
+
+# visualization tools
+# %matplotlib inline
+import matplotlib.pyplot as plt
+from cirq.contrib.svg import SVGCircuit
+
+
+def filter_36(x, y):
+    keep = (y == 3) | (y == 6)
+    x, y = x[keep], y[keep]
+    y = y == 3
+    return x,y
+
+def remove_contradicting(xs, ys):
+    mapping = collections.defaultdict(set)
+    # Determine the set of labels for each unique image:
+    for x,y in zip(xs,ys):
+       mapping[tuple(x.flatten())].add(y)
+    
+    new_x = []
+    new_y = []
+    for x,y in zip(xs, ys):
+      labels = mapping[tuple(x.flatten())]
+      if len(labels) == 1:
+          new_x.append(x)
+          new_y.append(list(labels)[0])
+      else:
+          # Throw out images that match more than one label.
+          pass
+    
+    num_3 = sum(1 for value in mapping.values() if True in value)
+    num_6 = sum(1 for value in mapping.values() if False in value)
+    num_both = sum(1 for value in mapping.values() if len(value) == 2)
+
+    print("Number of unique images:", len(mapping.values()))
+    print("Number of 3s: ", num_3)
+    print("Number of 6s: ", num_6)
+    print("Number of contradictory images: ", num_both)
+    print()
+    print("Initial number of examples: ", len(xs))
+    print("Remaining non-contradictory examples: ", len(new_x))
+    
+    return np.array(new_x), np.array(new_y)
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+# Rescale the images from [0,255] to the [0.0,1.0] range.
+x_train, x_test = x_train[..., np.newaxis]/255.0, x_test[..., np.newaxis]/255.0
+x_train, y_train = filter_36(x_train, y_train)
+x_test, y_test = filter_36(x_test, y_test)
+
+
+# Image resizing
+x_train_small = tf.image.resize(x_train, (4,4)).numpy()
+x_test_small = tf.image.resize(x_test, (4,4)).numpy()
+
+
+x_train_nocon, y_train_nocon = remove_contradicting(x_train_small, y_train)
+
+def convert_to_circuit(image):
+    """Encode truncated classical image into quantum datapoint."""
+    values = np.ndarray.flatten(image)
+    qubits = cirq.GridQubit.rect(4, 4)
+    circuit = cirq.Circuit()
+    for i, value in enumerate(values):
+        if value:
+            circuit.append(cirq.X(qubits[i]))
+    return circuit
+
+
+x_train_circ = [convert_to_circuit(x) for x in x_train_bin]
+x_test_circ = [convert_to_circuit(x) for x in x_test_bin]
+
+SVGCircuit(x_train_circ[0])
+# plt.imshow(x_test_small[0, :, :, 0])
+# plt.colorbar()
+# plt.show()
